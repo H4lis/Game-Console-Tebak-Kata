b5050258be3929aff8970644a6a187c451a227e2
h4lis <nurhalis092@gmail.com>
2023-07-05 08:12:45 +0800

    commit-ketiga

--- src/com/oop/Main.java
+++ src/com/oop/Main.java
@@ -2,50 +2,52 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String word = "sistem informasi";
-        char[] kata = new char[word.length()];
-        String kunci = "kita jurusan?";
-        int hitung = 0;
-        int salah = 0;
-        boolean isTrue = false;
+        String word = "sistem informasi"; // Kata yang akan ditebak
+        char[] kata = new char[word.length()]; // Array untuk menyimpan huruf-huruf yang ditebak
+        String kunci = "kita jurusan?"; // Frase kunci yang ditampilkan dalam permainan
+        int hitung = 0; // Penghitung tebakan yang benar
+        int salah = 0; // Penghitung tebakan yang salah
+        boolean isTrue = false; // Flag untuk memeriksa apakah kata sudah terbaca seluruhnya
 
         Scanner scanner = new Scanner(System.in);
 
+        // Menampilkan pengenalan permainan
         System.out.println("+--------------------------------------------------------------+");
         System.out.println("|                       GAME TEBAK KATA                         |");
         System.out.println("+--------------------------------------------------------------+");
         System.out.println("| Kesalahan hanya diberikan 3 kali. Jika melebihi, dinyatakan   |");
         System.out.println("| kalah.                                                        |");
         System.out.println("|                                                               |");
-        System.out.println("| Kata Kunci          : " + kunci );
+        System.out.println("| Kata Kunci          : " + kunci);
         System.out.println("| Tampilan Awal       : " + getFormattedWord(word, kata));
         System.out.println("+--------------------------------------------------------------+");
 
+        // Melakukan iterasi untuk setiap huruf dalam kata
         for (int y = 0; y < word.length(); y++) {
-            System.out.print("\nTebak huruf ke " + (y + 1) + ": ");
+            System.out.print("\nTebak huruf ke " + (y + 1) + ": "); // Minta pemain untuk menebak huruf
             char tebak = scanner.nextLine().charAt(0);
 
             hitung = 0;
             for (int c = 0; c < word.length(); c++) {
-                if (tebak == word.charAt(c)) {
-                    kata[c] = tebak;
-                    hitung++;
+                if (tebak == word.charAt(c)) { // Memeriksa apakah huruf yang ditebak cocok dengan huruf dalam kata
+                    kata[c] = tebak; // Memperbarui huruf yang ditebak dalam array 'kata'
+                    hitung++; // Menambahkan penghitung untuk tebakan yang benar
                 }
             }
 
             System.out.println("+--------------------------------------------------------------+");
             if (hitung < 1) {
                 salah++;
-                System.out.println("|  >>> Elo Salah! <<<   |   Jumlah kesalahan = " + salah + "  |");
+                System.out.println("|  >>> Salah! <<<   |   Jumlah kesalahan = " + salah + "  |"); // Tebakan salah
             } else {
-                System.out.println("|  >>> Good Job! <<<    |   Jumlah kesalahan = " + salah + "  |");
+                System.out.println("|  >>> Bagus! <<<    |   Jumlah kesalahan = " + salah + "  |"); // Tebakan benar
             }
-            System.out.println("|  Tampilan: " + getFormattedWord(word, kata));
+            System.out.println("|  Tampilan: " + getFormattedWord(word, kata)); // Menampilkan keadaan terkini kata
             System.out.println("+--------------------------------------------------------------+");
 
             if (salah > 2) {
                 System.out.println("\n\n<< MAAF KESEMPATAN ANDA SUDAH HABIS >>");
-                System.out.println("\n\n====================kalah yaa kasihan.....!!!====================");
+                System.out.println("\n\n====================Kalah yaa kasihan.....!!!===================="); // Pemain kalah dalam permainan
                 break;
             }
 
@@ -59,7 +61,7 @@ public class Main {
 
             if (isTrue) {
                 System.out.println("\n\n<< SELAMAT ANDA MENDAPATKAN 10 JUTA >>");
-                System.out.println("\n\n-------------------Congrulation-----------------");
+                System.out.println("\n\n-------------------Selamat-----------------"); // Pemain menang dalam permainan
                 break;
             }
         }
@@ -67,6 +69,7 @@ public class Main {
         scanner.close();
     }
 
+    // Mengembalikan kata yang telah diformat dengan huruf-huruf yang ditebak ditampilkan dan asterisk (*) untuk huruf-huruf yang belum terbaca
     private static String getFormattedWord(String word, char[] guessedLetters) {
         StringBuilder formattedWord = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
